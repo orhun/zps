eeec2cbbeafc7d824112d573bfb5b6ef9910c9a0
orhun <orhunparmaksiz@gmail.com>
2019-09-26 23:40:27 +0300

    Rename project and source files to 'zkill'

---
+++ src/zkill.c
@@ -0,0 +1,11 @@
+// gcc -O3 -Wall zkill.c -o zkill.o
+#include <stdio.h>
+#include <stdlib.h>
+#include "zkill.h"
+
+/*!
+ * Entry-point
+ */
+int main(int argc, char *argv[]) {
+    fprintf(stderr, "zkill v%s\n", VERSION);
+}
