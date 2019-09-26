0716cb695553b6210bb483b9aaa4e0ad91257923
orhun <orhunparmaksiz@gmail.com>
2019-09-27 00:18:24 +0300

    Add required declarations to header file

--- src/zkill.h
+++ src/zkill.h
@@ -1,1 +1,8 @@
-#define VERSION "1.0"
+
+#define VERSION "1.0"           /* Version */
+#define _XOPEN_SOURCE 700       /* POSIX.1-2008 + XSI (SuSv4) */
+#define _LARGEFILE64_SOURCE     /* Enable LFS */
+#define _FILE_OFFSET_BITS 64    /* Support 64-bit file sizes */
+#ifndef USE_FDS
+#define USE_FDS 15              /* Maximum number of file descriptors to use */
+#endif
