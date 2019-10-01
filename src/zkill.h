79bbf75d5d3fd506af5917dfa02c64badc8d45df
orhun <orhunparmaksiz@gmail.com>
2019-10-02 02:17:13 +0300

    Refactor zkill source header for improving stat check

--- src/zkill.h
+++ src/zkill.h
@@ -1,15 +1,15 @@
 
-#define VERSION "1.0"                     /* Version */
-#define _XOPEN_SOURCE 700                 /* POSIX.1-2008 + XSI (SuSv4) */
-#define _LARGEFILE64_SOURCE               /* Enable LFS */
-#define _FILE_OFFSET_BITS 64              /* Support 64-bit file sizes */
+#define VERSION "1.0"         /* Version */
+#define _XOPEN_SOURCE 700     /* POSIX.1-2008 + XSI (SuSv4) */
+#define _LARGEFILE64_SOURCE   /* Enable LFS */
+#define _FILE_OFFSET_BITS 64  /* Support 64-bit file sizes */
 #ifndef USE_FDS
-#define USE_FDS 15                        /* Maximum number of file descriptors to use */
+#define USE_FDS 15            /* Maximum number of file descriptors to use */
 #endif
-#define PROC_FS "/proc"                   /* '/proc' filesystem */
-#define STAT_FILE "/stat"                 /* Filename for status of PID */
-#define BLOCK_SIZE 4096                   /* Fixed block size for reading operations */
-#define PROCESS_DRST 0                    /* Return value of D/R/S/T process code */
-#define PROCESS_ZOMBIE 1                  /* Return value of Z (zombie/defunct) process code */
-#define PROCESS_READ_ERROR -1             /* Return value of read error on '/proc' */
-#define STATE_ZOMBIE "State:\tZ (zombie)" /* Status file entry of zombie state */
+#define PROC_FS "/proc"       /* '/proc' filesystem */
+#define STAT_FILE "/stat"     /* Filename for status of PID */
+#define BLOCK_SIZE 4096       /* Fixed block size*/
+#define PROCESS_DRST 0        /* Return value of D/R/S/T process code */
+#define PROCESS_ZOMBIE 1      /* Return value of Z (zombie/defunct) process code */
+#define PROCESS_READ_ERROR -1 /* Return value of read error on '/proc' */
+#define STATE_ZOMBIE "Z"      /* Status file entry of zombie state */
