41963800fcf795c47f9d2d80b932a0d20078eec9
orhun <orhunparmaksiz@gmail.com>
2019-10-02 13:11:02 +0300

    Add LICENSE information to the header file

--- src/zps.h
+++ src/zps.h
@@ -1,3 +1,21 @@
+/**!
+ * zps, <TODO: Add description>
+ * Copyright (C) 2019 by orhun <https://www.github.com/orhun>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #define VERSION "1.0"         /* Version */
 #define _XOPEN_SOURCE 700     /* POSIX.1-2008 + XSI (SuSv4) */
 #define _LARGEFILE64_SOURCE   /* Enable LFS */
