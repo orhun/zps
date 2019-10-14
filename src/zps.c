903dd3e05ce147dcdb0762ae067e22f19bdb2b1c
orhun <orhunparmaksiz@gmail.com>
2019-10-14 11:21:57 +0300

    Add project description & remove TODO

--- src/zps.c
+++ src/zps.c
@@ -1,5 +1,5 @@
 /**!
- * zps, <TODO: Add description>
+ * zps, a small utility for listing or cleaning up zombie processes.
  * Copyright (C) 2019 by orhun <https://www.github.com/orhun>
  *
  * This program is free software: you can redistribute it and/or modify
