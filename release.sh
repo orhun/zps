4a28d1927f6b64fbedd3194f123eff50b2277da6
orhun <orhunparmaksiz@gmail.com>
2020-08-10 19:35:44 +0300

    Use double quotes for variables in release script

--- release.sh
+++ release.sh
@@ -2,23 +2,23 @@
 
 # Create tag
 read -p "Tag: " tag
-git tag -s -a $tag
+git tag -s -a "$tag"
 git push --tags
 
 # Create assets
 make
 cd build/
-tar czvf zps-$tag.tar.gz zps
-shasum -a 512 zps-$tag.tar.gz > zps-$tag.sha512
-gpg --detach-sign zps-$tag.tar.gz # 0xB928720AEC532117
+tar czvf "zps-$tag.tar.gz" zps
+shasum -a 512 "zps-$tag.tar.gz" > "zps-$tag.sha512"
+gpg --detach-sign "zps-$tag.tar.gz" # 0xB928720AEC532117
 
 # Upload assets
 # https://github.com/buildkite/github-release
-gh-release "v$tag" zps-$tag.* \
+gh-release "v$tag" "zps-$tag".* \
     --tag "$tag" \
     --github-repository "orhun/zps" \
 
 # Clean up
-rm zps-$tag.*
+rm "zps-$tag".*
 cd ..
 echo "New release locked and ready (v$tag)"
