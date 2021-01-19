3d96a4a346a2db871ff4d367ef47c0a6b3c84e02
orhun <orhun@archlinux.org>
2021-01-19 22:33:31 +0300

    Fix the asset locations

--- release.sh
+++ release.sh
@@ -9,7 +9,7 @@ git push --tags
 # Create assets
 make
 cd build/ || exit
-tar -czvf "zps-$tag.tar.gz" zps README.md LICENSE man/zps.1
+tar -czvf "zps-$tag.tar.gz" zps ../README.md ../LICENSE ../man/zps.1
 gpg --local-user "B928720AEC532117" \
     --detach-sign "zps-$tag.tar.gz"
 shasum -a 512 "zps-$tag.tar.gz" > "zps-$tag.tar.gz.sha512"
@@ -21,6 +21,6 @@ gh-release "v$tag" "zps-$tag".* \
     --github-repository "orhun/zps" \
 
 # Clean up
-rm -f "zps-$tag".*
+#rm -f "zps-$tag".*
 cd .. || exit
 echo "New release locked and ready (v$tag)"
