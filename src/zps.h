7ba30e16add797606c18a87674a62cb63fd7e03b
orhun <orhun@archlinux.org>
2021-06-27 21:52:28 +0300

    Update version variables (for 1.2.7 release)

--- src/zps.h
+++ src/zps.h
@@ -15,7 +15,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#define VERSION "1.2.5"            /* Version */
+#define VERSION "1.2.7"            /* Version */
 #define _XOPEN_SOURCE 700          /* POSIX.1-2008 + XSI (SuSv4) */
 #define _LARGEFILE64_SOURCE        /* Enable LFS */
 #define _FILE_OFFSET_BITS 64       /* Support 64-bit file sizes */
