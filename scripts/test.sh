8b095076f5fa90236293bd7f7495620fcef49355
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-11-07 14:25:38 +0300

    Use GitHub Actions for running tests

---
+++ scripts/test.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+project_dir="$(pwd)/.."
+# Compile the example & run
+cd "${project_dir}/example"
+gcc -O3 -Wall zproc.c -o z.o
+./z.o &>/dev/null &
+./z.o &>/dev/null &
+# Compile the main source & run
+cd "${project_dir}/src"
+gcc -fprofile-arcs -ftest-coverage -s -O3 -Wall -Wextra -pedantic zps.c -o zps
+./zps -v && ./zps -h && printf '1' | ./zps -p
+./zps -x && ./zps -lr && ./zps -f
+./zps -r -f 20 && ./zps -s
+# Print code coverage information
+gcov zps.c
+# Send report to codecov
+[ "$UPLOAD" == 'true' ] && bash <(curl -s https://codecov.io/bash)
+# Cleanup
+rm -v zps zps.c.* zps.gc* stdlib.h.gcov
