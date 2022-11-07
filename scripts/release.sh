5673d3cf46a21a2c871fcb4acea87a14e13f0cb6
Orhun Parmaksız <orhunparmaksiz@gmail.com>
2022-11-07 20:54:25 +0300

    Move release.sh to scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,15 +2,16 @@
 
 set -e
 
+project_dir="$(pwd)/.."
+
 # Create tag
-read -p "Tag: " tag
-git -c user.signingkey="B928720AEC532117" \
-    tag -s -a "$tag"
-git push --tags
+read -rp "Tag: " tag
+git -c user.signingkey="B928720AEC532117" tag -s -a "$tag"
 
 # Create assets
+cd "$project_dir"
 make
-cd build/ || exit
+cd "$project_dir/build/" || exit
 tar -czvf "zps-$tag.tar.gz" zps ../README.md \
     ../LICENSE ../man/zps.1 zps.desktop
 gpg --local-user "B928720AEC532117" \
@@ -23,7 +24,10 @@ gh-release "v$tag" "zps-$tag".* \
     --tag "$tag" \
     --github-repository "orhun/zps" \
 
+# Push tag
+git push --tags
+
 # Clean up
-rm -f "zps-$tag".*
+#rm -f "zps-$tag".*
 cd .. || exit
 echo "New release locked and ready (v$tag)"
