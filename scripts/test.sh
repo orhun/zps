2ca978e091d465c1f94c5528bc89c543056d2555
Matthäus Wininger <matthaeus dot wininger at univie dot ac dot at>
2023-12-18 05:43:17 +0100

    Remove the other `-f` test

--- scripts/test.sh
+++ scripts/test.sh
@@ -12,7 +12,7 @@ gcc -O3 -Wall zproc.c -o z.o
 cd "${project_dir}/src"
 gcc -fprofile-arcs -ftest-coverage -s -O3 -Wall -Wextra -pedantic zps.c -o zps
 ./zps -v && ./zps -h && printf '1' | ./zps -p
-./zps -x && ./zps -lr && ./zps -f
+./zps -x && ./zps -lr
 ./zps -r && ./zps -s
 # Print code coverage information
 gcov zps.c
